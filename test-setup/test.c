342058389f35cf443ec40fcbcbd45c561e21fe9f
Saroar Jahan <msaroar.dev@gmail.com>
2026-04-08 16:41:47 +0600

    c programmin learning

---
+++ test-setup/test.c
@@ -0,0 +1,51 @@
+// #include <stdio.h>
+// int main(){
+//     int num;
+//     scanf("%d", &num);
+
+//     if(num == 0){
+//         printf("Zero");
+//     } else {
+//         printf("Non Zero");
+//     }
+//     return 0;
+// }
+
+// #include <stdio.h>
+// int main(){
+//     int num;
+//     scanf("%d", &num);
+
+//     int add = 5;
+
+//     int sum = num + add;
+//     printf("%d", sum);
+//     return 0;
+// }
+
+// #include <stdio.h>
+// int main(){
+//     int num1;
+//     scanf("%d", &num1);
+
+//     int num2;
+//     scanf("%d", &num2);
+
+//     if(num1 % num2 == 0 || num2 % num1 == 0){
+//         printf("Yes");
+//     } else {
+//         printf("No");
+//     }
+
+//     return 0;
+// }
+
+#include <stdio.h>
+int main()
+{
+    float num;
+    scanf("%f", &num);
+
+    printf("%.3f", num);
+    return 0;
+}
