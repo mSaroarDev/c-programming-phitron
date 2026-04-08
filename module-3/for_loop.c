b3f29f97dce34a6fbb5779191f654e91fc5464db
Saroar Jahan <msaroar.dev@gmail.com>
2026-04-08 17:51:26 +0600

    Add C program to print odd numbers from 1 to 100 and update project files

---
+++ module-3/for_loop.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main(){
+  // print Hello C programming 100 times
+  // for (int i = 1; i <=100; i++) {
+  //   printf("Hello C Programming\n");
+  // }
+
+  // print 1-100 
+  // for(int i = 1; i <=100; i++){
+  //   printf("%d\n", i);
+  // }
+
+  // print 1-100 even numbers 
+  // for(int i = 2; i <=100; i=i+2){
+  //   printf("%d\n", i);
+  // }
+
+  // print 1-100 odd numbers 
+  for(int i = 1; i <=100; i=i+2){
+    printf("%d\n", i);
+  }
+}
