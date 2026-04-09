03dee4dc0373cb831d592ea7b81dc944f669b028
Saroar Jahan <msaroar.dev@gmail.com>
2026-04-09 16:59:36 +0600

    ccc

---
+++ module-3/practice-day/variables.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+  int a;
+  long long int b;
+  double c;
+  char d;
+
+  scanf("%d %lld %lf %c", &a, &b, &c, &d);
+
+  printf("%d\n%lld\n%.2lf\n%c", a, b, c, d);
+  
+  return 0;
+}
