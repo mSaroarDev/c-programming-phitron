81aab86aee50c14fabb1dcff7757affc60650d53
Saroar Jahan <msaroar.dev@gmail.com>
2026-04-10 19:01:29 +0600

    cc

---
+++ assignment_1/shopping.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main()
+{
+  long long int n;
+  scanf("%lld", &n);
+
+  if(n > 1000){
+    printf("I will buy Punjabi\n");
+
+    long long int leftAmount = n - 1000;
+
+    if(leftAmount >= 500){
+      printf("I will buy new shoes\n");
+      printf("Alisa will buy new shoes\n");
+    }
+  } else {
+    printf("Bad luck!\n");
+  }
+
+  return 0;
+}
