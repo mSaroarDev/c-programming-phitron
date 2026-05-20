f4871832f90841b6d35ccd9f39acbaa06f1ce9f1
Saroar Jahan <msaroar.dev@gmail.com>
2026-05-20 17:59:55 +0600

    daf

---
+++ _cpp/first_code.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  // std:: cout << "Hello world" << std:: endl; 
+  // std:: cout << 1000;
+
+  int x;
+  cin >> x;
+
+  cout << x;
+  return 0;
+}
