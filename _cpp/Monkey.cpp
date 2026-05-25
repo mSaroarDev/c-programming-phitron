9ab26613d045f448103c4f3e7d04a2ef05cd86a9
Saroar Jahan <msaroar.dev@gmail.com>
2026-05-25 18:49:58 +0600

    Add implementation for Monkey problem from HackerRank

---
+++ _cpp/Monkey.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+  string str;
+  while (getline(cin, str))
+  {
+    string word = str;
+    sort(word.begin(), word.end());
+
+    // cout << word << endl;
+    while(word)
+    {
+      cout << c;
+    }
+  }
+  cin.ignore();
+
+  return 0;
+}
