2b7f8273fd0cfb3a8d9860a98ebe4e62b37fff69
Saroar Jahan <msaroar.dev@gmail.com>
2026-05-25 20:33:19 +0600

    Add implementations for Choose Three and Who Is It problems from HackerRank; update Monkey problem implementation

--- _cpp/Monkey.cpp
+++ _cpp/Monkey.cpp
@@ -2,17 +2,21 @@
 using namespace std;
 int main()
 {
-  string str;
-  while (getline(cin, str))
+  char str[100001];
+  while (cin.getline(str, 100001))
   {
     string word = str;
     sort(word.begin(), word.end());
 
-    // cout << word << endl;
-    while(word)
+    for (int i = 0; i < word.size(); i++)
     {
-      cout << c;
+      if (word[i] != ' ')
+      {
+        cout << word[i] << "";
+      }
     }
+
+    cout << endl;
   }
   cin.ignore();
 
---
+++ _cpp/Who_Is_It.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Student
+{
+  public:
+  int roll;
+  string name;
+  char section;
+  int num;
+};
+
+int main()
+{
+  int sl;
+  cin >> sl;
+
+  string name;
+  cin >> name;
+
+  char section;
+  cin >> section;
+
+  int num;
+  cin >> num;
+
+  Student 
+  return 0;
+}
