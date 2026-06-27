dbff1d12396228ab0a9db42acd750be04c15be49
Saroar Jahan <msaroar.dev@gmail.com>
2026-06-27 14:39:21 +0600

    Refactor range sum query implementation for efficiency and update output format

--- Y_Range_sum_query.cpp
+++ Y_Range_sum_query.cpp
@@ -3,23 +3,35 @@ using namespace std;
 int main()
 {
   int n, q;
-  cin >> n >> q;  
+  cin >> n >> q;
 
-  vector<int> v(4);
-  for (int i = 0; i < n; i++)   
+  vector<long long int> v(n + 1);
+  for (int i = 1; i <= n; i++)
   {
     cin >> v[i];
   }
 
-  for (int i = 0; i < q; i++)   // o(q*n)
+  vector<long long int> pre(n + 1);
+  pre[1] = v[1];
+  for (int i = 2; i <= n; i++)
   {
-    int left, right;
-    cin >> left >> right;      
+    pre[i] = pre[i - 1] + v[i];
+  }
 
-    int sum = 0;
-    for (int i = left - 1; i < right; i++)   
+  while (q--)
+  {
+    int l, r;
+    cin >> l >> r;
+
+    long long int sum = 0;
+
+    if (l == 1)
     {
-      sum = sum + v[i];
+      sum = pre[r];
+    }
+    else
+    {
+      sum = pre[r] - pre[l - 1];
     }
 
     cout << sum << endl;
